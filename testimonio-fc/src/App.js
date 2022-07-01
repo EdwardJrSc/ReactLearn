6ab6d4d99a25b35e0e215729d522627e1294bf40
unknown <Sistemas@ALUBAQ-PRTL-03.alubaq.local>
2022-07-01 15:01:39 -0500

    COMMIT 4

---
+++ testimonio-fc/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Testimonial from './components/testimonial';
+
+function App() {
+  return (
+    <div className="App">
+      <Testimonial />
+    </div>
+  );
+}
+
+export default App;
