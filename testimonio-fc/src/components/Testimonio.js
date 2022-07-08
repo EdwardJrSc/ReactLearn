88f06bd1d368372e004196a360d4799c90be8341
unknown <Sistemas@ALUBAQ-PRTL-03.alubaq.local>
2022-07-07 21:26:48 -0500

    COMMIT 8

--- testimonio-fc/src/components/Testimonio.js
+++ testimonio-fc/src/components/Testimonio.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import '../styles/testimonio.css';
 
-function Testimonio () {
+function Testimonio (props) {
   return (
     <div className='contenedor-testimonio'>
       <img
         className='imagen-testimonio'
-        src={require('../images/Emma.png')}
+        src={require(`../images/${props.imagen}.png`)}
         alt='Foto de Emma'
       />
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
-        <p className='cargo-testimonio'>Ingeniera de software en spotify</p>
-        <p className='texto-testimonio'>"Siempre me ha costado aprender JavaScript. Tomé muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como un ingeniero de software en Spotify".</p>
+        <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
+        <p className='texto-testimonio'>"{props.testimonio}".</p>
       </div>
     </div>
   );
